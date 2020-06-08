9bfb91d33ec870cf01fd85da579c977195224959
Shriji <shrijike@gmail.com>
2020-06-08 12:19:49 +0530

    mobile fixes

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,14 @@ let template = (e) => {
             <div class="card">
                     <div class="oyh"><img src="./${e.type[0]}/${e.file}.${e.type[0]}" class="card-img-top img-fluid" loading="lazy"></div>
                 <div class="card-body py-2">
-                  <p class="card-text card-title">${e.desc} </p>
+                  <p class="card-text card-title">${e.desc}  <span class="supported-formats">${e.type} </span> </p>
                 </div>
               </div>   </a>                     
         </div>
 `
 }
 let codes;
+
 var settings = {
     "url": "./data.json",
     "method": "GET",
@@ -21,25 +22,7 @@ var settings = {
 $.ajax(settings).done(function (response) {
     codes = response.codes;
     codes.forEach(e => {
+        $("#avaiableCodes").append(` <span>${e.code}</span> `)
         $("#entry").append(template(e))
     });
 });
-
-
-
-var p = 90;
-let t;
-//linear-gradient(90deg, #f7b733, #fc4a1a)
-function timer() {
-    p += .5;
-    if (p > 360) {
-        p = 0;
-    }
-    $(".btn-cta").css('background', `linear-gradient(${p}deg, #060606, #424242)`)
-}
-$(".btn-cta").mouseover(function () {
-    t = setInterval(timer, .5);
-}).mouseleave(function () {
-    clearInterval(t)
-    $(".btn-cta").css('background', `linear-gradient(${p}deg, #060606, #424242)`)
-})
